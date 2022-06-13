409d4251f2143d65c93184f8f6fc656a0bdf700c
psjCom <psj@bestco.kr>
2022-06-13 14:48:00 +0900

    Implement Main entry point
    실행될 메인 로직 , 정렬 알고리즘사용

---
+++ facam-java-oop/src/test/java/com/facam/javaoop/MainTest.java
@@ -0,0 +1,19 @@
+package com.facam.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void main(){
+        //Given
+        String[] args={"3","1","2"};
+        //When
+        Main.main(args);
+        //Then
+
+
+    }
+
+}
