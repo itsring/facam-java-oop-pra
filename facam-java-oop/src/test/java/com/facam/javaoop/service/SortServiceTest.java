0990f81dc0633d3b4f94dfac48601ae9e7d464eb
psjCom <psj@bestco.kr>
2022-06-13 15:17:20 +0900

    Implement SortService
    정렬 구현체를 사용하는 서비스를 별도로 작성

---
+++ facam-java-oop/src/test/java/com/facam/javaoop/service/SortServiceTest.java
@@ -0,0 +1,23 @@
+package com.facam.javaoop.service;
+
+import com.facam.javaoop.logic.BubbleSort;
+import com.facam.javaoop.logic.JavaSort;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortServiceTest {
+    private SortService sut = new SortService(new BubbleSort<String>());
+    @Test
+    void test() {
+        //Given
+
+        //When
+        List<String> actual = sut.doSort(List.of("3","2","1"));
+        //Then
+        assertEquals(List.of("1","2","3"),actual);
+
+    }
+}
